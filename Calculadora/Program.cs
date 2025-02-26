7713e845b4e68365343be86e4bf3e3b5216b2b62
0wendell <wendell160326@gmail.com>
2025-02-26 12:00:45 -0300

    Criação da base master

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Base da seleção de operação
+            
+            Console.WriteLine("Qual tipo de operação você quer usar? ");
+            Console.WriteLine("1 - Soma ");
+            Console.WriteLine("2 - Subtração ");
+            Console.WriteLine("3 - Multiplicação ");
+            Console.WriteLine("4 - Divisão\n ");
+
+            Console.ReadLine();
+
+            int operacao = int.Parse(Console.ReadLine());
+
+            // Números solicitados pelo usuario
+
+            Console.WriteLine("Digite o primerio número");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número");
+            int num2 = int.Parse(Console.ReadLine());
+
+            // Realização das operações
+
+            int resultado;
+
+
+
+
+
         }
     }
 }
