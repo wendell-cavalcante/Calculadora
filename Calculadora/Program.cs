3bae2a76b5e252462c8f11c038cc9a684a026717
wendell-cavalcante <wendell160326@gmail.com>
2025-02-28 19:31:43 -0300

    Opção de continuar calculando

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -4,91 +4,121 @@
     {
         static void Main(string[] args)
         {
-            // Base da seleção de operação
-
-            Console.WriteLine("Qual tipo de operação você quer usar? ");
-            Console.WriteLine("1 - Soma ");
-            Console.WriteLine("2 - Subtração ");
-            Console.WriteLine("3 - Multiplicação ");
-            Console.WriteLine("4 - Divisão\n ");
-
-            double operacao = double.Parse(Console.ReadLine());
-
-            // Opção Inválida
-
-            while (operacao > 4 || operacao < 1)
+            bool continuarCalculando = true;
+            do
             {
-                Console.Clear();
-                Console.WriteLine("Opção inválida, escolha uma opção válida:\n ");
+                // Base da seleção de operação
+
+                Console.WriteLine("Qual tipo de operação você quer usar?\n ");
                 Console.WriteLine("1 - Soma ");
                 Console.WriteLine("2 - Subtração ");
                 Console.WriteLine("3 - Multiplicação ");
                 Console.WriteLine("4 - Divisão\n ");
-                operacao = double.Parse(Console.ReadLine());
-                Console.Clear();
 
-            }
+                double operacao = double.Parse(Console.ReadLine());
 
-            // Exibição da função que vai usar
+                // Opção Inválida
 
-            if (operacao == 1) Console.WriteLine($"\nFunção SOMA \n");
-            if (operacao == 2) Console.WriteLine($"\nFunção SUBTRAÇÃO \n");
-            if (operacao == 3) Console.WriteLine($"\nFunção MULTIPLICAÇÃO \n");
-            if (operacao == 4) Console.WriteLine($"\nFunção DIVISÃO \n");
+                while (operacao > 4 || operacao < 1)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Opção inválida, escolha uma opção válida:\n ");
+                    Console.WriteLine("1 - Soma ");
+                    Console.WriteLine("2 - Subtração ");
+                    Console.WriteLine("3 - Multiplicação ");
+                    Console.WriteLine("4 - Divisão\n ");
+                    operacao = double.Parse(Console.ReadLine());
+                    Console.Clear();
 
-            // Números solicitados pelo usuario
+                }
 
-            Console.Write("Digite o primeiro número: ");
-            double num1 = double.Parse(Console.ReadLine());
+                // Exibição da função que vai usar
 
-            Console.Write("Digite o segundo número: ");
-            double num2 = double.Parse(Console.ReadLine());
+                if (operacao == 1) Console.WriteLine($"\nFunção SOMA \n");
+                if (operacao == 2) Console.WriteLine($"\nFunção SUBTRAÇÃO \n");
+                if (operacao == 3) Console.WriteLine($"\nFunção MULTIPLICAÇÃO \n");
+                if (operacao == 4) Console.WriteLine($"\nFunção DIVISÃO \n");
 
-            // Realização das operações
+                // Números solicitados pelo usuario
 
-            double resultado;
+                Console.Write("Digite o primeiro número: ");
+                double num1 = double.Parse(Console.ReadLine());
 
-            switch (operacao)
-            {
-                case 1:
+                Console.Write("Digite o segundo número: ");
+                double num2 = double.Parse(Console.ReadLine());
+
+                // Realização das operações
+
+                double resultado;
+
+                switch (operacao)
+                {
+                    case 1:
+                        {
+                            resultado = num1 + num2;
+                            Console.WriteLine($"\nSeu resultado é: {resultado}");
+                        }
+                        break;
+
+                    case 2:
+                        {
+                            resultado = num1 - num2;
+                            Console.WriteLine($"\nSeu resultado é: {resultado}");
+                        }
+                        break;
+
+                    case 3:
+                        {
+                            resultado = num1 * num2;
+                            Console.WriteLine($"\nSeu resultado é: {resultado}");
+                        }
+                        break;
+
+                    case 4:
+                        if (num2 != 0)
+                        {
+                            resultado = num1 / num2;
+                            Console.WriteLine("\nResultado da divisão: " + resultado);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Erro! Divisão por zero não permitida.");
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Operação inválida!");
+                        break;
+                        
+                }
+
+                string resposta;
+                bool entradaInvalida = false;
+                do
+
+                {
+                    Console.WriteLine("Deseja continuar calculando? Sim/Não");
+                    resposta = Console.ReadLine();
+                    if (resposta.ToLower() == "sim" || resposta.ToLower() == "não")
+                        
                     {
-                        resultado = num1 + num2;
-                        Console.WriteLine($"\nSeu resultado é: {resultado}");
-                    }
-                    break;
-
-                case 2:
-                    {
-                        resultado = num1 - num2;
-                        Console.WriteLine($"\nSeu resultado é: {resultado}");
-                    }
-                    break;
-
-                case 3:
-                    {
-                        resultado = num1 * num2;
-                        Console.WriteLine($"\nSeu resultado é: {resultado}");
-                    }
-                    break;
-
-                case 4:
-                    if (num2 != 0)
-                    {
-                        resultado = num1 / num2;
-                        Console.WriteLine("\nResultado da divisão: " + resultado);
+                        entradaInvalida = true;
                     }
                     else
                     {
-                        Console.WriteLine("Erro! Divisão por zero não permitida.");
+                        Console.WriteLine("Entrada Inválida. Digite 'Sim' ou 'Não'");
                     }
-                    break;
-                default:
-                    Console.WriteLine("Operação inválida!");
-                    break;
-            }
+
+                    Console.Clear();
+
+                } while (!entradaInvalida);
+
+                if (resposta.ToLower() == "não")
+                {
+                    continuarCalculando = false;
+                }
+
+            } while (continuarCalculando);
+            Console.WriteLine("Calculadora encerrada, obrigado por usar.");
         }
     }
 }
-
-
-
